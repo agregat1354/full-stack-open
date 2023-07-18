7d94438628d94f95fcce58d7065499b62886aae2
agregat1354 <icollectreports@gmail.com>
2023-07-18 11:31:46 +0200

    complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,52 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Filter = ({filterValue, handleChange}) => (
+const Filter = ({ filterValue, handleChange }) => (
   <>
-    filter: <input value={filterValue} onChange={handleChange}/>
+    filter: <input value={filterValue} onChange={handleChange} />
   </>
 )
 
-const PersonForm = ({nameValue, handleNameChange, numberValue, handleNumberChange, handleSubmit}) => {
+const PersonForm = ({ nameValue, handleNameChange, numberValue, handleNumberChange, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
-      name: <input value={nameValue} onChange={handleNameChange}/>
-      <br/>
-      number: <input value={numberValue} onChange={handleNumberChange}/>
-      <br/>
+      name: <input value={nameValue} onChange={handleNameChange} />
+      <br />
+      number: <input value={numberValue} onChange={handleNumberChange} />
+      <br />
       <button type="submit">add</button>
-    </form> 
+    </form>
   )
 }
 
-const Person = ({name, number}) => <p>{name} {number}</p>
+const Person = ({ name, number }) => <p>{name} {number}</p>
 
-const Persons = ({persons}) => {
-  return(
+const Persons = ({ persons }) => {
+  return (
     <>
-      {persons.map(person => <Person key={person.name} name={person.name} number={person.number}/>)}
+      {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
     </>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(persons.find(elem => elem.name === newName)) {
+    if (persons.find(elem => elem.name === newName)) {
       alert(`${newName} already exists in the phonebook!`)
       return
-    } 
-    setPersons([...persons, {name: newName, number: newNumber}])
+    }
+    setPersons([...persons, { name: newName, number: newNumber }])
     setNewName('')
     setNewNumber('')
   }
@@ -57,12 +58,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filterValue={filter} handleChange={(e)=>setFilter(e.target.value)}/>
+      <Filter filterValue={filter} handleChange={(e) => setFilter(e.target.value)} />
       <h2>Add new</h2>
       <PersonForm handleSubmit={handleSubmit} nameValue={newName} numberValue={newNumber}
-       handleNameChange={e=>setNewName(e.target.value)} handleNumberChange={e=>setNewNumber(e.target.value)}/>
+        handleNameChange={e => setNewName(e.target.value)} handleNumberChange={e => setNewNumber(e.target.value)} />
       <h2>Numbers</h2>
-      <Persons persons={personsToShow}/>
+      <Persons persons={personsToShow} />
     </div>
   )
 }
