4e28cd9e2f3582143f82f257bf955f5c6c4db75b
agregat1354 <icollectreports@gmail.com>
2023-07-25 08:24:39 +0200

    move blog model to it's own module

--- part4/app.js
+++ part4/app.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require("./utils/config.js")
+const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog.js')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
