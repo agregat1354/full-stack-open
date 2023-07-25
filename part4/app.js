3826f02b51b2917a234d9a61343c3b49b516a3ff
agregat1354 <icollectreports@gmail.com>
2023-07-25 09:03:30 +0200

    add logger module and use it in other modules

--- part4/app.js
+++ part4/app.js
@@ -4,19 +4,18 @@ const cors = require('cors')
 const config = require("./utils/config.js")
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs.js')
-
+const logger = require('./utils/logger.js')
 
 const Blog = require('./models/blog.js')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
-    .then(() => console.log("succesfully connected to mongodb"))
-    .catch(error => console.log("failed to connect to mongodb", error))
+    .then(() => logger.info("succesfully connected to mongodb"))
+    .catch(error => logger.info("failed to connect to mongodb", error))
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
-
 
 module.exports = app
