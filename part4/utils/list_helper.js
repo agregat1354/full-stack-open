d632816a978e7d07673b9a3f23aa1a6e4f055f73
agregat1354 <icollectreports@gmail.com>
2023-07-25 10:28:57 +0200

    add tests for totalLikes function

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,9 @@ const dummy = blogs => {
     return 1
 }
 
+const totalLikes = blogs => blogs.length > 0 ? blogs.reduce((sum, currElem) => sum += currElem.likes, 0) : 0
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
