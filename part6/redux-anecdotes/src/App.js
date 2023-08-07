4831152221e1d4c3f6fb06a595b266862c226473
agregat1354 <icollectreports@gmail.com>
2023-08-07 11:06:26 +0200

    add upvoting, adding new anecdotes and sorting

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,34 +1,45 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
+import { upvoteAnecdote, newAnecdote } from "./reducers/anecdoteReducer.js";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) =>
+    state.toSorted((a, b) => b.votes - a.votes)
+  );
+  const dispatch = useDispatch();
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    console.log("vote", id);
+    dispatch(upvoteAnecdote(id));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newAnecdoteContent = e.target.Anecdote.value.trim();
+    dispatch(newAnecdote(newAnecdoteContent));
+    e.target.Anecdote.value = "";
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" name="Anecdote" />
+        </div>
+        <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,29 +1,49 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
-  }
-}
+    votes: 0,
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case "UPVOTE":
+      return state.map((vote) =>
+        vote.id !== action.payload ? vote : { ...vote, votes: vote.votes + 1 }
+      );
+    case "ADD_NEW_ANECDOTE":
+      return [...state, asObject(action.payload)];
+    default:
+      return state;
+  }
+};
 
-  return state
-}
+const upvoteAnecdote = (id) => {
+  return {
+    type: "UPVOTE",
+    payload: id,
+  };
+};
 
-export default reducer
+const newAnecdote = (content) => {
+  return {
+    type: "ADD_NEW_ANECDOTE",
+    payload: content,
+  };
+};
+
+export { reducer as default, upvoteAnecdote, newAnecdote };
